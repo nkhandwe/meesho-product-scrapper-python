a12acd9616d8a70a0e13420178f4db8f8f8bb4b9
krishnajhapate <ki54.jhapate@gmail.com>
2021-09-24 13:45:14 +0530

    added field names

--- exportcsv.py
+++ exportcsv.py
@@ -2,7 +2,33 @@ from productclass import CsvOperations
 
 file_name="products.csv"
 field_names=[
-    ''
+    'Product name',
+    'Description',
+    'Slug',
+    'SKU',
+    'Categories',
+    'Auto Generate SKU',
+    'Status',
+    'Is featured?',
+    'Brand',
+    'Product collections',
+    'Labels',
+    'Tax',
+    'Images',
+    'Price',
+    'Product attributes',
+    'Import type',
+    'Is variation default?',
+    'Stock status',
+    'With storehouse management',
+    'Allow checkout when out of stock',
+    'Sale price',
+    'Start date sale price',
+    'End date sale price',
+    'Weight',
+    'Length',
+    'Wide',
+    'Height',
 ]
 
 csv = CsvOperations(file_name=file_name,data=Product name
